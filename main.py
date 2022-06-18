02ca958cf0668fa658f029cfcf0b637e97e1a5d3
Vilsijain <93503754+Vilsijain@users.noreply.github.com>
2022-06-18 19:23:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def generate_appended_strings(words):
 
 
 @app.route('/replace/<string:word>/')
-@cors_origin() 
+@corss_origin() 
 def generate_replaced_strings(word):
     final =  [char for char in word]
 

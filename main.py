5d2d7fb2567b854c52c75016635de97c1ed0b3b1
Raghav Gupta <ragaws1674@gmail.com>
2022-06-18 20:34:15 +0530

    Merge all functions in one endpoint

--- main.py
+++ main.py
@@ -3,23 +3,29 @@ import os
 from flask import Flask,jsonify
 from flask_cors import cross_origin
 
-from utils import * 
+from utils import (
+        generate_appended_strings,
+        generate_prepended_strings,
+        generate_replaced_strings,
+        generate_synonyms,
+        generate_tlds,
+)
 
 app = Flask(__name__)
 
 @app.route("/<string:word>",methods=["GET","OPTIONS"])
 @cross_origin()
 def generate_all(word):
-    generators = [generate_synonyms,
-                 generate_appended_strings,
-                 generate_prepended_strings,
-                 generate_replaced_strings,
-                 generate_tlds
-                ]
-    result = []
-    for gen in generators: 
-        result.extend(gen(word))
-    print(result)
+    generators = {
+                    'synonyms': generate_synonyms,
+                    'append': generate_appended_strings,
+                    'prepend': generate_prepended_strings,
+                    'replaced': generate_replaced_strings,
+                    'tlds': generate_tlds
+                }
+    result = {}
+    for op,fn in generators.items(): 
+            result[op] = fn(word)
     return jsonify(result)
     
 if __name__ == '__main__':
